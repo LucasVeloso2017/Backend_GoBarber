decf503139266da87649ef698cbfc3b499c0d7bc
lucas <lucas.velloso2017@gmail.com>
2020-12-07 22:13:14 -0300

    deploy on heroku

--- dist/config/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    jwt: {
-        secret: String(process.env.APP_SECRET),
-        //secret:"asdasdasdasd",
-        expiresIn: '1d'
-    }
-};
